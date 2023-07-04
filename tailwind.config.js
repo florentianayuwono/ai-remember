f8f5e8706e58bbf90ecea08d8e054eb401bbe4ef
florentianayuwono <florentianayuwono@gmail.com>
2023-07-04 15:04:39 +0800

    Configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "",
+        secondary: "",
+        tertiary: "",
+        "black-100": "",
+        "black-200": "",
+        "white-100": "",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-pattern": "",
+      }
+    }
   },
   plugins: [],
-}
+};
 
