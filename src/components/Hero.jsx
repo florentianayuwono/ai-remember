db8dbd47eadb69a7d705d744cf5e37be0ab8003a
florentianayuwono <e0851439@u.nus.edu>
2023-07-27 17:19:36 +0800

    Add hero background gradient

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { TypeAnimation } from "react-type-animation";
 
 const Hero = () => {
   return (
-    <section className="relative w-full h-screen mx-auto">
+    <section className="relative w-full h-screen mx-auto hero-background">
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
       >
