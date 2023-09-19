389d56caa532be40b896f9e18602951245f7c3f2
florentianayuwono <e0851439@u.nus.edu>
2023-09-19 16:21:45 +0800

    Fix margin

--- src/components/landing_page/WhyUs.jsx
+++ src/components/landing_page/WhyUs.jsx
@@ -7,27 +7,24 @@ import { SectionWrapper } from "../../hoc";
 import { reasons } from "../../constants";
 import { fadeIn, textVariant } from "../../utils/motion";
 
-const ReasonCard = ({
-  index,
-  name,
-  description,
-  image,
-}) => {
+const ReasonCard = ({ index, name, description, image }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
         options={{ max: 45, scale: 1, speed: 450 }}
         className=" bg-white bg-opacity-90 p-5 rounded-2xl w-full my-5"
       >
-      <div className="flex">
-      <div className="text-[100px] mx-10">{image}</div>
-        <div className="mt-5">
-          <h3 className="text-purple-900 font-bold text-2xl sm:text-3xl">{name}</h3>
-          <p className="mt-2 text-secondary-brown text-md sm:text-lg">{description}</p>
+        <div className="flex m-5">
+          <div className="text-[100px]">{image}</div>
+          <div className="m-2">
+            <h3 className="text-purple-900 font-bold text-2xl sm:text-3xl">
+              {name}
+            </h3>
+            <p className="mt-2 text-secondary-brown text-md sm:text-lg">
+              {description}
+            </p>
+          </div>
         </div>
-      </div>
-
-        
       </Tilt>
     </motion.div>
   );
@@ -38,7 +35,9 @@ const WhyUs = () => {
     <>
       <motion.div variants={textVariant()}>
         <h2 className={styles.sectionHeadText}>Why AI-Remember?</h2>
-        <p className="mt-3 text-primary-lightpink text-[17px] max-w-3xl leading-[30px]">Because it's incredible! Here is why we love it...</p>
+        <p className="mt-3 text-black text-[17px] max-w-3xl leading-[30px]">
+          Because it's incredible! Here is why we love it...
+        </p>
       </motion.div>
 
       <div className="mt-20 flex flex-col">
