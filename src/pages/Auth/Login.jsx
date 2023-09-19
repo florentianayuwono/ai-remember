930b41ecd950c9212816ca2af0a250c5afcda980
tyx021 <yxting1109@gmail.com>
2023-09-19 17:18:29 +0800

    abstract out InputForm

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,23 +1,11 @@
 import { useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
-import {} from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
-
+import InputForm from "../../components/common/InputForm";
 import { auth } from "../../firebase_setup/FirebaseConfig";
 import { logo2, googleicon } from "../../assets";
 import CircularIndicator from "../../components/CircularIndicator";
-
-const InputForm = ({ title, htmlValue, value, handleChange, placeholder }) => {
-  return (
-    <div className="mb-4 ">
-      <label className="inline-block text-gray-700 mb-2" htmlFor={htmlValue}>
-        {title}
-      </label>
-      <input className="bg-white border rounded-xl w-full py-2 px-3 placeholder:text-gray-400 text-gray-900" id={htmlValue} type={htmlValue} placeholder={placeholder} value={value} onChange={handleChange} autoComplete="off" />
-    </div>
-  );
-};
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -62,23 +50,23 @@ const Login = () => {
         <img className="w-24 m-auto mb-4" src={logo2} />
         <InputForm title="Email" value={email} htmlValue="email" handleChange={handleEmailChange} placeholder="your email*" />
         <InputForm title="Password" value={password} htmlValue="password" handleChange={handlePasswordChange} placeholder="password *" />
-        <div className="flex items-center justify-between mt-4 mb-2">
-          <button className=" bg-purple-500 hover:bg-purple-700 text-white w-full  py-2 px-4 rounded-3xl" type="submit" onClick={handleSubmit}>
+        <div className="flex items-center mt-4 mb-2">
+          <button className=" bg-purple-500 hover:bg-purple-700 text-white w-full h-10 py-2 px-4 rounded-3xl" type="submit" onClick={handleSubmit}>
             {loginLoading ? <CircularIndicator /> : "Login"}
           </button>
         </div>
-        <p>
+        <p className="text-center">
           Don't have an account yet?{" "}
-          <a href="#" className="text-purple-500">
+          <a href="/signup" className="text-purple-500">
             Sign Up!
           </a>
         </p>
-        <a href="#" className="text-purple-500 flex items-center">
+        <a href="/forget" className="text-purple-500 text-center">
           Forgot Password?
         </a>
 
         <div className="mt-8 flex items-center justify-between">
-          <button className="outline outline-purple-500 bg-white hover:bg-purple-400 text-black w-full  py-2 px-4 rounded-3xl flex items-center" type="submit" onClick={handleSubmit}>
+          <button className="outline outline-purple-500 bg-white hover:bg-purple-400 text-black w-full py-2 px-4 rounded-3xl flex items-center" type="submit" onClick={handleSubmit}>
             <img src={googleicon} className="w-6 mr-4" />
             Continue with Google
           </button>
