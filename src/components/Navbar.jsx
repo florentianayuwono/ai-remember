dd86bf8b7034be5fe1ad58eb70db2a17d042d799
tyx021 <yxting1109@gmail.com>
2023-09-19 13:09:41 +0800

    Add Link to login page for "Try Now" button under Pricing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 hero-background`}
-    >
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 hero-background`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to="/"
@@ -29,13 +27,7 @@ const Navbar = () => {
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10 items-center">
           {navLinks.map((link) => (
-            <li
-              key={link.id}
-              className={`${
-                active === link.title ? "text-white" : "text-primary-lightpink"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
-              onClick={() => setActive(link.title)}
-            >
+            <li key={link.id} className={`${active === link.title ? "text-white" : "text-primary-lightpink"} hover:text-white text-[18px] font-medium cursor-pointer`} onClick={() => setActive(link.title)}>
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
@@ -43,26 +35,13 @@ const Navbar = () => {
         </ul>
 
         <div className="sm:hidden flex flex-1 justify-end items-center">
-          <img
-            src={toggle ? close : menu}
-            alt="menu"
-            className="w-[28px] h-[28px] object-contain cursor-pointer"
-            onClick={() => setToggle(!toggle)}
-          />
-          <div
-            className={`${
-              !toggle ? "hidden" : "flex"
-            } p-6 violet-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
-          >
+          <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={() => setToggle(!toggle)} />
+          <div className={`${!toggle ? "hidden" : "flex"} p-6 violet-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
             <ul className="list-none flex justify-end items-start flex-col gap-4">
               {navLinks.map((link) => (
                 <li
                   key={link.id}
-                  className={`${
-                    active === link.title
-                      ? "text-white"
-                      : "text-primary-lightpink"
-                  } hover:text-white text-[16px] font-poppins font-medium cursor-pointer`}
+                  className={`${active === link.title ? "text-white" : "text-primary-lightpink"} hover:text-white text-[16px] font-poppins font-medium cursor-pointer`}
                   onClick={() => {
                     setToggle(!toggle);
                     setActive(link.title);
@@ -83,12 +62,7 @@ const Navbar = () => {
 const LoginButton = () => {
   return (
     <Link to="/login">
-      <button
-        onClick={() => null}
-        className=" rounded-full bg-primary-pink py-2 px-6 font-medium transition-transform transform hover:scale-110"
-      >
-        Login
-      </button>
+      <button className=" rounded-full bg-primary-pink py-2 px-6 font-medium transition-transform transform hover:scale-110">Login</button>
     </Link>
   );
 };
