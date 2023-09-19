74103e454d16fe27d0be79c8a67c068577dad15a
Xu Yi <E0772330@u.nus.edu>
2023-09-19 13:24:46 +0800

    Rename home to landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,9 @@
-import Hero from "../components/home_page/Hero";
-import Navbar from "../components/home_page/Navbar";
-import Features from "../components/home_page/Features";
-import OthersWords from "../components/home_page/OthersWords";
-import WhyUs from "../components/home_page/WhyUs";
-import Pricing from "../components/home_page/Pricing";
+import Hero from "../components/landing_page/Hero";
+import Navbar from "../components/landing_page/Navbar";
+import Features from "../components/landing_page/Features";
+import OthersWords from "../components/landing_page/OthersWords";
+import WhyUs from "../components/landing_page/WhyUs";
+import Pricing from "../components/landing_page/Pricing";
 
 const Home = () => {
   return (
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
 import Login from "./Auth/Login";
-import Home from "./Home";
+import Home from "./Landing";
 
 export { Home, Login };
