af697e02aef599e3e8f91c4f5067ed240fd1ee3c
Xu Yi <E0772330@u.nus.edu>
2023-09-22 22:15:10 +0800

    Add conversation page template

--- src/pages/Conversation.jsx
+++ src/pages/Conversation.jsx
@@ -1,6 +1,7 @@
 import HomeNavbar from "../components/common/HomeNavbar";
 import ReactGA from "react-ga4";
 import { useEffect } from "react";
+import { logo2 } from "../assets";
 
 const Conversation = () => {
 
@@ -8,12 +9,26 @@ const Conversation = () => {
     ReactGA.send({ hitType: "pageview", page: "/conversation", title: "Conversation Page" });
   }, [])
 
+  const Bearly = () => {
+    return (
+    <div className="absolute mt-16 text-secondary-brown">
+        <div className=" min-w-[100px] max-w-[300px] w-[100px] m-6 flex flex-col justify-center items-center">
+          <img src={logo2} alt="AI-Bear"/>
+          <div className=" pt-2 italic font-bold text-lg">
+            Bearly
+          </div>
+        </div>
+      </div>);
+  }
+
 
   return (
-    <>
+    <div className="relative z-0 h-screen bg-contain bg-note-paper text-secondary-brown overflow-y-scroll">
       <HomeNavbar />
-      <div>Conversation</div>
-    </>
+      <Bearly />
+
+
+    </div>
   );
 };
 
