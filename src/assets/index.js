e824159e64a86b66c977e6480a68fbe9624937b8
Xu Yi <E0772330@u.nus.edu>
2023-09-26 21:58:18 +0800

    Remove unnecessary assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,15 +10,7 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 import googleicon from "./googleicon.svg";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
-import soconnect from "./soconnect.png";
-import groundzero from "./groundzero.png";
-import classroom360 from "./classroom360.jpg";
-import digitalit from "./digitalit.png";
-
 import loveletter from "./loveletter.png";
 import crush from "./crush.webp";
 
-export { logo, logo2, crush, backend, creator, mobile, web, github, menu, close, carrent, jobit, soconnect, groundzero, classroom360, digitalit, tripguide, loveletter, googleicon };
+export { logo, logo2, crush, backend, creator, mobile, web, github, menu, close, loveletter, googleicon };
