e6c967295a1740cf4db3268aa6b4a8eccc6d0ca0
Xu Yi <E0772330@u.nus.edu>
2023-09-26 20:45:11 +0800

    Change alert dialog template

--- src/components/common/AlertDialog.jsx
+++ src/components/common/AlertDialog.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
 
-const AlertDialog = ({handleModelChange, title, description, hasCancel, image}) => {
-    const navigate = useNavigate();
-
-    const handleChange = () => {
-    }
+const AlertDialog = ({handleModelChange, handleChange, title, description, hasCancel, image}) => {
 
   return (
     <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
@@ -17,20 +12,23 @@ const AlertDialog = ({handleModelChange, title, description, hasCancel, image}) 
         <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
           <div className="sm:flex sm:items-start">
             <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-              <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">🌟 Upgrade to Pro for an Enhanced Experience! 🌟</h3>
+              <img scr={image} alt="" />
+              <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">{title}</h3>
               <div className="mt-2">
-                <p className="text-sm text-gray-500">Ready to take your memories to the next level?</p>
+                <p className="text-sm text-gray-500">{description}</p>
               </div>
             </div>
           </div>
         </div>
-        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+        <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 justify-center">
           <button 
           onClick={handleChange}
           type="button" className="inline-flex w-full justify-center rounded-md hover:bg-primary-purple px-3 py-2 text-sm font-semibold text-white shadow-sm bg-secondary-darkpurple sm:ml-3 sm:w-auto">Upgrade to Pro</button>
+         { hasCancel &&
           <button 
           onClick={handleModelChange}
           type="button" className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
+         }
         </div>
       </div>
     </div>
