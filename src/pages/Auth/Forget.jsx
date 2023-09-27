879b6b38761a3f3b474471ff6cdb4413541f3cfb
Xu Yi <86706511+optionalemon@users.noreply.github.com>
2023-09-27 21:40:19 +0800

    Add forget password firebase (#48)

--- src/pages/Auth/Forget.jsx
+++ src/pages/Auth/Forget.jsx
@@ -1,21 +1,39 @@
 import { logo2 } from "../../assets";
 import { useEffect, useState } from "react";
+import { auth } from "../../firebase_setup/FirebaseConfig";
 import InputForm from "../../components/common/InputForm";
 import ReactGA from "react-ga4";
+import { useSendPasswordResetEmail } from "react-firebase-hooks/auth";
+import { LANDING_PAGE, LOGIN_PAGE } from "../../constants";
+import toast from "react-hot-toast";
+import { CircularIndicator } from "../../components";
 
 const Forget = () => {
   const [email, setEmail] = useState("");
+  const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(auth);
 
   useEffect(() => {
     ReactGA.send({ hitType: "pageview", page: "/forget", title: "Forget Password Page" });
   }, [])
 
+  const actionCodeSettings = {
+    url: LANDING_PAGE,
+    handleCodeInApp: true,
+  };
+
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async(e) => {
     e.preventDefault();
+    const success = await sendPasswordResetEmail(
+      email,
+      actionCodeSettings
+    );
+    if (success) {
+      toast.success("Email sent!")
+    }
   };
 
   return (
@@ -23,9 +41,10 @@ const Forget = () => {
       <form className="bg-white rounded-xl px-14 py-8 text-sm flex flex-col justify-center ">
         <img className="w-24 m-auto mb-4" src={logo2} />
         <InputForm title="Email" value={email} htmlValue="email" handleChange={handleEmailChange} placeholder="your email*" />
+        {error !== undefined && <div className="text-red-500">{error.message}</div>}
         <div className="flex items-center justify-between mt-4 mb-2">
-          <button className=" bg-purple-500 hover:bg-purple-700 text-white w-full h-10 py-2 px-4 rounded-3xl" type="submit" onClick={handleSubmit}>
-            Reset Password
+          <button className=" bg-purple-500 flex justify-center items-center hover:bg-purple-700 text-white w-full h-10 py-2 px-4 rounded-3xl" type="submit" onClick={handleSubmit}>
+            {sending? <CircularIndicator /> : "Reset Password"}
           </button>
         </div>
       </form>
