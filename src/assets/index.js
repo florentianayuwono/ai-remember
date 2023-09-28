8ebe2b553056a63060fa6fb41860fcd7c3c50d9a
florentianayuwono <e0851439@u.nus.edu>
2023-09-28 22:12:14 +0800

    Clean up

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,17 +1,9 @@
 import logo from "./logo.png";
 import logo2 from "./logo2.png";
-
-import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
-import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 import googleicon from "./googleicon.svg";
-
-import loveletter from "./loveletter.png";
 import crush from "./crush.webp";
 import sleepybear from "./sleep_logo.png"
 
-export { logo, logo2, crush, backend, creator, mobile, web, github, menu, close, loveletter, googleicon, sleepybear };
+export { logo, logo2, crush, menu, close, googleicon, sleepybear };
